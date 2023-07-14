19d9ba6d829c828d7179feb075caac6516d22ec4
Jeklah <arthurbowers@gmail.com>
2023-07-14 11:57:12 +0000

    Adding add_one function.

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -6,8 +6,16 @@ fn main() {
     };
 
     println!("The value of y is : {y}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is {x}.");
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
