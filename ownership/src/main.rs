6ab3a2c1e74c9e8a1519a280605099e8d37bbca0
arthur bowers <arthur.bowers@phabrix.com>
2023-07-19 16:29:32 +0100

    Updating files on ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,8 +2,13 @@ fn main() {
     let a_num = 4;
     make_and_drop();
     let first = String::from("Ferris");
-    let full = add_suffix(first);
-    println!("{full}");
+    // let full = add_suffix(first);
+    // println!("{full}");
+
+    let first_clone = first.clone();
+    let full = add_suffix(first_clone);
+
+    println!("{full}, originally: {first}");
 }
 
 fn make_and_drop() {
