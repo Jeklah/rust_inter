106cf1fd3a9f233d82c812ef609c6a680095679c
arthur bowers <arthur.bowers@phabrix.com>
2023-07-20 10:45:57 +0100

    Updating with notes about ownership.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -9,6 +9,24 @@ fn main() {
     let full = add_suffix(first_clone);
 
     println!("{full}, originally: {first}");
+
+    // Which of these compile compile?
+    // A)
+    let b = Box::new(0);
+    let b2 = b;
+    move_a_box(b);
+    // B)
+    let b = Box::new(0);
+    move_a_box(b);
+    println!("{b}");
+    // C)
+    let b = Box::new(0);
+    move_a_box(b);
+    let b2 = b;
+    // D)  Only D will compile as the ownership of b is moved to b2 or when it is passed to move_a_box
+    let b = Box::new(0);
+    let b2 = b;
+    move_a_box(b2);
 }
 
 fn make_and_drop() {
@@ -19,3 +37,5 @@ fn add_suffix(mut name: String) -> String {
     name.push_str(" Jr.");
     name
 }
+
+fn move_a_box(b: Box<i32>) {}
