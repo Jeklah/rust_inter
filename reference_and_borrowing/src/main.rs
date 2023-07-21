e8dd5299e5f82b4d46dbe13018a975b6397d8cb1
arthur bowers <arthur.bowers@phabrix.com>
2023-07-21 09:25:49 +0100

    Adding references to greet function.
    Adding ambersands to signify referencing.

--- reference_and_borrowing/src/main.rs
+++ reference_and_borrowing/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let m1 = String::from("Hello");
     let m2 = String::from("World");
-    greet(m1, m2);
+    greet(&m1, &m2);
     let s = format!("{m1}, {m2}!");
 }
 
-fn greet(g1: String, g2: String) {
+fn greet(g1: &String, g2: &String) {
     println!("{g1}, {g2}!");
 }
